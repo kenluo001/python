c4f32ff6c624ac52d3a35ce0836624fbc0ceebd5
“kenluo001” <ken.luo.5227@gmail.com>
2023-09-28 19:35:29 +0800

    update python base

--- python base/hello.py
+++ python base/hello.py
@@ -1,6 +1,17 @@
+'''
+Author: “kenluo001” “ken.luo.5227@gmail.com”
+Date: 2023-09-16 16:29:26
+LastEditors: “kenluo001” “ken.luo.5227@gmail.com”
+LastEditTime: 2023-09-28 19:35:08
+FilePath: /python/python base/hello.py
+Description: 
+
+Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+'''
 
 # print hello world
 
 print('hello world')
 print("hello world")
 
+print("123 abc")
